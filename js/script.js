a1688f41a1b6aa881b186d715941784561d075e3
Maxime-Belleuvre <102807688+Maxime-Belleuvre@users.noreply.github.com>
2024-06-21 01:13:53 +0200

    with jd better

---
+++ js/script.js
@@ -0,0 +1,67 @@
+function getSpells(url){
+
+    fetch(url)
+    .then(res=>res.json())
+    .then(data => {
+        document.getElementById("spellMono").addEventListener("click", (e) => SelectSpell(e,data.spells,"mono"))
+        document.getElementById("spellMulti").addEventListener("click", (e) => SelectSpell(e,data.spells,"multi"))
+        document.getElementById("spellGeneral").addEventListener("click", (e) => SelectSpell(e,data.spells,"general"))
+    })
+}
+
+
+getSpells("./json/data.json")
+
+function modifyImgSpells(url){
+    document.getElementById("spellImg").src=`../images/${url}`
+}
+function modifyNameSpells(e){
+    document.getElementById("hearderText").textContent = e;
+}
+function modifyCostSpells(e){
+    document.getElementById("cost").textContent = e;
+}
+function modifyDescriptionSpells(e){
+    document.getElementById("description").textContent = e;
+}
+function modifyDefenseSpells(e){
+    document.getElementById("defense").textContent = e;
+}
+
+function modifySpells(url,title,cost,description,defense){
+    document.getElementById("cardContainer").classList.add("active");
+    modifyImgSpells(url);
+    modifyNameSpells(title);
+    modifyCostSpells(cost);
+    modifyDescriptionSpells(description);
+    modifyDefenseSpells(defense);
+}
+
+function SelectSpell(e,data,target){    
+    let newArray = [];
+    if(target === "mono" || target === "multi"){
+        const array = [];
+        data.forEach((spell)=>{
+            if(spell.targets === `${target}`){
+                array.push(spell)
+            }
+        })
+        newArray = arrayRandom(array);
+        console.log(newArray);
+        
+    }else{
+        newArray = arrayRandom(data);
+        console.log(newArray);
+    }
+    modifySpells(newArray.url,newArray.title,newArray.cost,newArray.description,newArray.defense)
+}
+
+
+function arrayRandom(array){
+    return array[Math.floor(Math.random()*array.length)];
+}
+
+
+
+
+
